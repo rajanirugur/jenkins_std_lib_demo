ad3d04bab4f20a7cd5129be25094aea4bb3cb5a9
Rajan <rajan@rathinamtrainers.com>
2021-08-08 08:47:04 +0000

    Modified dockerbuild step

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,7 +1,7 @@
 def call(Map config) {
 	sh "docker build -t ${config.dockerAccountName}/${config.dockerImageName}:${env.BUILD_ID} ."
 	sh "docker tag ${config.dockerAccountName}/${config.dockerImageName}:${env.BUILD_ID} ${config.dockerAccountName}/${config.dockerImageName}:latest"
-	withCredentials([string(credentialsId: '${config.dockerHubCredentials}', variable: 'DOCKER_HUB_PASSWORD')]) {
+	withCredentials([string(credentialsId: 'DOCKER_HUB_CREDENTIALS', variable: 'DOCKER_HUB_PASSWORD')]) {
 	    sh "docker login -u ${config.dockerAccountName} -p ${DOCKER_HUB_PASSWORD} docker.io"
 	}
 	sh "docker push ${config.dockerAccountName}/${config.dockerImageName}:${env.BUILD_ID}"
