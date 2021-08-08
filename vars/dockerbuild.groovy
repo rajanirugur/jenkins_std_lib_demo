c45f286e0818761b72190504e1068fae4822da0e
Rajan <rajan@rathinamtrainers.com>
2021-08-08 08:38:09 +0000

    Modified dockerbuild step

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,13 +1,13 @@
 def call(Map config) {
 	sh "docker build -t ${config.dockerAccountName}/${config.dockerImageName}:${env.BUILD_ID} ."
-	sh "docker tag rathinamtrainers/myjavaapp:${env.BUILD_ID} rathinamtrainers/myjavaapp:latest"
+	sh "docker tag ${config.dockerAccountName}/myjavaapp:${env.BUILD_ID} ${config.dockerAccountName}/myjavaapp:latest"
 	withCredentials([string(credentialsId: 'DOCKER_HUB_CREDENTIALS', variable: 'DOCKER_HUB_PASSWORD')]) {
-	    sh "docker login -u rathinamtrainers -p ${DOCKER_HUB_PASSWORD} docker.io"
+	    sh "docker login -u ${config.dockerAccountName} -p ${DOCKER_HUB_PASSWORD} docker.io"
 	}
-	sh "docker push rathinamtrainers/myjavaapp:${env.BUILD_ID}"
-	sh "docker push rathinamtrainers/myjavaapp:latest"
+	sh "docker push ${config.dockerAccountName}/myjavaapp:${env.BUILD_ID}"
+	sh "docker push ${config.dockerAccountName}/myjavaapp:latest"
 	sh 'docker logout docker.io'
-	sh "docker image rm  rathinamtrainers/myjavaapp:${env.BUILD_ID}"
-	sh "docker image rm  rathinamtrainers/myjavaapp:latest"
+	sh "docker image rm  ${config.dockerAccountName}/myjavaapp:${env.BUILD_ID}"
+	sh "docker image rm  ${config.dockerAccountName}/myjavaapp:latest"
 }
 
